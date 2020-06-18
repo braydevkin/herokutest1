0fc3607f046556c18cb4d5177156d940db2c66de
brayan <brayan.molk5@gmail.com>
2020-06-18 19:40:15 -0300

    DBREV04

--- server.js
+++ server.js
@@ -23,28 +23,22 @@ app.post("/newagent", (req, res) => {
 
     const intentname = req.body.queryResult.intent.displayName;
 
-    if (intentname == 'boas-vindas') {
-        res.json({"fulfillmentText": "Para cadastrar digite, 'Cadastrar' "})
-    }
+    if (intentname == 'cadastrar') {
+        var userName = request.body.queryResult.parameters['user_name']
+        var userEmail = request.body.queryResult.parameters['user_email']
 
-    else if (intentname == 'cadastrar') {
-        async function saveUser() {
-            var userName = request.body.queryResult.parameters['user_name']
-            var userEmail = request.body.queryResult.parameters['user_email']
-
-            try {
-                const newUser = await registerModel.create({
-                    name: userName,
-                    email: userEmail
-                })
-            }
-            catch{
-                res.json({ "fulfillmentText": "Houve um erro no seu cadastro" })
-            }
+        try {
+            registerModel.create({
+                name: userName,
+                email: userEmail
+            })
         }
-        saveUser()
-        res.json({ "fulfillmentText": "Cadastro efetuado com sucesso !" })
+        catch{
+            res.json({ "fulfillmentText": "Houve um erro no seu cadastro" })
+        }
     }
+    saveUser()
+    res.json({ "fulfillmentText": "Cadastro efetuado com sucesso !" })
 })
 // listen for requests :)
 const listener = app.listen(process.env.PORT || 3000, () => {
