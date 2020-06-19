d9ff49f584603f88c0f218996fac5161a1d7c41f
brayan <brayan.molk5@gmail.com>
2020-06-19 13:54:11 -0300

    restruturação dos arquivos

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const mongoose = require('mongoose')
 const bodyParser = require("body-parser")
 const cors = require('cors')
-const routes = require('./routes')
+const routes = require('./src/routes')
 //----------------------------------------------------------- CALL
 const app = express();
 app.use(cors())
